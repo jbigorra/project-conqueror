0cfa718f45092a98cd21df87d35dd441d9431d67
Juan bigorra <10943260+jbigorra@users.noreply.github.com>
2025-07-17 19:09:44 +0200

    chore: Update testing configuration for Behave
    - Replaces the placeholder test script in package.json with "vitests" for improved testing functionality.
    - Adds type checking configuration in vitest.config.mjs, enabling TypeScript checks with a specified tsconfig file for the Behave project.

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -7,6 +7,11 @@ export default defineConfig({
         test: {
           name: "behave",
           include: ["packages/behave/tests/**/*.test.ts"],
+          typecheck: {
+            tsconfig: "packages/behave/tsconfig.json",
+            checker: "tsc",
+            enabled: true,
+          },
         },
       },
     ],
