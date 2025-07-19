d2d1c5c9cb2a8b5eac25d9fea54c3465ca2b3484
Juan bigorra <10943260+jbigorra@users.noreply.github.com>
2025-07-19 21:55:07 +0200

    feat: Create new package called "lib" to contain common utilities and useful patterns to be used by the apps and other packages

---
+++ packages/lib/vitest.config.mjs
@@ -0,0 +1,20 @@
+import path from "node:path";
+import { defineConfig } from "vitest/config";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  plugins: [tsconfigPaths()],
+  resolve: {
+    alias: {
+      "@/lib": path.resolve(__dirname, "./src"),
+    },
+  },
+  test: {
+    name: "lib",
+    typecheck: {
+      tsconfig: "./tsconfig.json",
+      checker: "tsc",
+      enabled: true,
+    },
+  },
+});
