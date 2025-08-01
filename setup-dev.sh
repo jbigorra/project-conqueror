cc7a1dbeedb09e4e4aed8e9547a3a6955e0376f4
Juan bigorra <10943260+jbigorra@users.noreply.github.com>
2025-08-01 16:49:33 +0200

    chore: remove setup-dev.sh and update turbo.json and package.json for improved build process
    - Deleted setup-dev.sh as it was redundant.
    - Updated turbo.json to ensure the lib package is built before others by modifying the dependsOn configuration.
    - Added dev:setup script in package.json for the lib package to facilitate development.

--- setup-dev.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-# Pre-build dependencies between internal packages.
-# NOTE: Avoid excesive use of dependencies between internal packages.
-#       This is mostly used for shared types, generics and useful patterns that other packages can use.
-turbo build --filter=@prj-conq/lib
-
