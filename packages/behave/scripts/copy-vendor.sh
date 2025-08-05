4708fba15c64f2f364e597e9ca2705dacd905b62
Juan bigorra <10943260+jbigorra@users.noreply.github.com>
2025-08-05 16:55:22 +0200

    chore: enhance copy-vendor script for improved vendor file management
    - Added echo statements for better visibility during the execution of the copy-vendor script.
    - Created the vendor directory structure before copying the code-maat-1.0.4-standalone.jar file to ensure proper organization.

--- packages/behave/scripts/copy-vendor.sh
+++ packages/behave/scripts/copy-vendor.sh
@@ -3,5 +3,13 @@
 # Copy the code-maat-1.0.4-standalone.jar file to the dist directory
 # This is needed because the code-maat-1.0.4-standalone.jar file is not included in the package
 # and is required to run the code_maat analysis
+
+pwd
+
+echo "Creating dist/infrastructure/code_maat/vendor directory"
 mkdir -p ./dist/infrastructure/code_maat/vendor
+
+echo "Copying code-maat-1.0.4-standalone.jar to dist/infrastructure/code_maat/vendor"
 cp ./src/infrastructure/code_maat/vendor/code-maat-1.0.4-standalone.jar ./dist/infrastructure/code_maat/vendor/code-maat-1.0.4-standalone.jar
+
+echo "Done copying vendor files!!"
