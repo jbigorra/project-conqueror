b9e5cd7d3133fde66815607221174800133fe3e7
Juan bigorra <10943260+jbigorra@users.noreply.github.com>
2025-08-20 21:24:55 +0200

    [ATM-3] feat: implement styles for the upload component.
    - Add instructions on how to generate git log file.
    - Add animated file icon and drop/upload area.
    - Set styles and animation to the upload button.

--- apps/webapp/src/features/upload/presentation/ui/upload.page.tsx
+++ apps/webapp/src/features/upload/presentation/ui/upload.page.tsx
@@ -10,29 +10,44 @@ export function UploadPage(props: PropsWithChildren) {
       `}
       title="Upload your files"
     >
-      <div class="box">
         <div class="row">
           <div class="col-xs-12 col-md-12">
-            <pre>
-              cd path/to/your/repo <br/><br/>
-              git log --all --numstat --date=short --pretty=format:'--%h--%ad--%aN' --no-renames --after=YYYY-MM-DD
-                &gt; logfile.log
+            <p>1. Execute the commands below to generate a git log file.</p>
+            <pre class="terminal">
+              <span class="user">user@laptop</span>&nbsp;<span class="path">~</span>
+              <br/>
+              <span class="path">&rarr;&nbsp;</span><span class="output">cd path/to/your/repo</span>
+              <br/>
+              <br/>
+              <span class="user">user@laptop</span>&nbsp;<span class="path">~/path/to/your/repo</span>
+              <br/>
+              <span class="path">&rarr;&nbsp;</span>
+                <span class="output">
+                  git log --all --numstat --date=short --pretty=format:'--%h--%ad--%aN' --no-renames --after=YYYY-MM-DD
+                  &gt; logfile.log
+                </span>
             </pre>
+            <p>2. Upload the git log file.</p>
           </div>
         </div>
-        <form id="form"
-          hx-post="/upload"
-          hx-encoding="multipart/form-data"
-          hx-target="#upload-response"
-          hx-swap="afterend"
-        >
-          <label for="file">Upload a git log file</label>
-          <input id="file" type="file" name="file" />
-          <button>Upload</button>
-        </form>
-        <progress id='progress' value='0' max='100'></progress>
-        <div id="upload-response"></div>
-      </div>
+        <div class="row">
+          <div class="col-xs-12 center-xs">
+            <form id="form"
+              hx-post="/upload"
+              hx-encoding="multipart/form-data"
+              hx-target="#upload-response"
+              hx-swap="afterend"
+            >
+              <div aria-label="Upload a git log file" class="drop-area">
+                <img src="upload/img/anim-file.svg" alt="animated file" />
+                <span>Drop your git log file here or click to browse.</span>
+                <input id="file" type="file" name="file" />
+              </div>
+              <button>Upload</button>
+            </form>
+            <div id="upload-response"></div>
+          </div>
+        </div>
     </Layout>
   )
 };
