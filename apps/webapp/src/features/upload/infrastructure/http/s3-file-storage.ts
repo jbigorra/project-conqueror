92cf6d87b5c2e2a7d5f7386c026d351c94a89825
Juan bigorra <10943260+jbigorra@users.noreply.github.com>
2025-09-02 15:59:52 +0200

    feat: Add error handling to S3FileStorage upload method and implement corresponding tests

--- apps/webapp/src/features/upload/infrastructure/http/s3-file-storage.ts
+++ apps/webapp/src/features/upload/infrastructure/http/s3-file-storage.ts
@@ -6,10 +6,18 @@ export class S3FileStorage implements IFileStorage {
   constructor(private readonly s3Client: S3Client) {}
 
   async upload(file: File): Promise<Result<void>> {
-    const fileContent = await file.text();
-    await this.s3Client.write(file.name, fileContent);
+    try {
+      const fileContent = await file.text();
+      await this.s3Client.write(file.name, fileContent);
 
-    return Result.success(undefined);
+      return Result.success(undefined);
+    } catch (e: any) {
+      const error = new Error(`${S3FileStorage.name}: upload failed`, {
+        cause: e.cause,
+      });
+      error.code = "OBJECT_STORAGE_ERROR";
+      return Result.error(error);
+    }
   }
 
   async download(file: File): Promise<Result<void>> {
---
+++ apps/webapp/tests/features/upload/infrastructure/http/s3-file-storage.test.ts
@@ -0,0 +1,41 @@
+import { S3FileStorage } from "#upload/infrastructure/http/s3-file-storage.ts";
+import { S3Client } from "bun";
+import { mockFn } from "bun-automock";
+import { describe, expect, it } from "bun:test";
+
+describe.only("S3FileStorage", () => {
+  it("should upload a file to S3 compatible object storage", async () => {
+    const s3Client = mockFn<S3Client>();
+    const fileStorage = new S3FileStorage(s3Client);
+    const file = new File(["Test content"], "test.log", {
+      type: "text/plain",
+    });
+
+    const result = await fileStorage.upload(file);
+
+    expect(result.isSuccess()).toBe(true);
+    expect(s3Client.write.spy()).toHaveBeenCalledWith(
+      "test.log",
+      "Test content",
+    );
+  });
+
+  it("should return error result when uploading file throws an error", async () => {
+    const s3Client = mockFn<S3Client>();
+    const fileStorage = new S3FileStorage(s3Client);
+    const error = new Error("Upload failed", { cause: "some error" });
+    (error as any).code = "OBJECT_STORAGE_ERROR";
+    s3Client.write.mockRejectedValue(error);
+    const file = new File(["Test content"], "test.log", {
+      type: "text/plain",
+    });
+
+    const result = await fileStorage.upload(file);
+
+    expect(result.isError()).toBe(true);
+    expect(result.getError().code).toBe("OBJECT_STORAGE_ERROR");
+    expect(result.getError().message).toBe(
+      `${S3FileStorage.name}: upload failed`,
+    );
+  });
+});
