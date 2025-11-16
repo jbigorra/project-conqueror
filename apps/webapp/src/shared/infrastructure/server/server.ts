923957b632c66dbd6ed82a1c24fa003717766bc2
Juan bigorra <10943260+jbigorra@users.noreply.github.com>
2025-11-16 16:54:55 +0100

    fix(): Fix type issues for Eden, stop using DI when creating the server

--- apps/webapp/src/shared/infrastructure/server/server.ts
+++ apps/webapp/src/shared/infrastructure/server/server.ts
@@ -5,73 +5,45 @@ import { staticPlugin } from "@elysiajs/static";
 import { swagger } from "@elysiajs/swagger";
 import { Elysia } from "elysia";
 import logixlysia from "logixlysia";
-import { LoggerInstance } from "../logging/logger";
-import { type BaseController } from "./controller-factory";
 import { indexController } from "./index.controller";
 
-export interface IStartServerDeps {
-  httpLogger: typeof logixlysia;
-  customLogger: ReturnType<typeof LoggerInstance.get>;
-  openapi: typeof swagger;
-  opentelemetry: typeof opentelemetry;
-  static: typeof staticPlugin;
-  controllers: BaseController[];
-}
-
-export const createServer = (app: Elysia, deps: IStartServerDeps) => {
-  return (
-    app
-      /**
-       * The plugins for the server are those that don't need to expose
-       * a decorator in the controllers.
-       */
-      .use(
-        deps.httpLogger({
-          config: {
-            showStartupMessage: false,
-            timestamp: {
-              translateTime: "yyyy-mm-dd HH:MM:ss",
-            },
-            ip: true,
-            logFilePath: "./logs/app.log",
-            logFilter: {
-              level: ["INFO", "WARN", "ERROR", "DEBUG"],
-            },
-          },
-        }),
-      )
-      .use(
-        deps.openapi({
-          path: "/openapi",
-          documentation: {
-            info: {
-              title: packageJson.name,
-              version: packageJson.version,
-              description: packageJson.description,
-            },
-          },
-        }),
-      )
-      .use(deps.opentelemetry())
-      .use(
-        deps.static({
-          assets: "src/assets",
-          prefix: "/",
-          noCache: process.env.NODE_ENV === "development",
-        }),
-      )
-      // Controllers
-      .use(deps.controllers)
-  );
-};
-
-export const server = createServer(new Elysia(), {
-  httpLogger: logixlysia,
-  customLogger: LoggerInstance.get(),
-  openapi: swagger,
-  opentelemetry: opentelemetry,
-  static: staticPlugin,
-  controllers: [indexController, uploadFilesController],
-});
+export const server = new Elysia()
+  .use(
+    logixlysia({
+      config: {
+        showStartupMessage: false,
+        timestamp: {
+          translateTime: "yyyy-mm-dd HH:MM:ss",
+        },
+        ip: true,
+        logFilePath: "./logs/app.log",
+        logFilter: {
+          level: ["INFO", "WARN", "ERROR", "DEBUG"],
+        },
+      },
+    }),
+  )
+  .use(
+    swagger({
+      path: "/openapi",
+      documentation: {
+        info: {
+          title: packageJson.name,
+          version: packageJson.version,
+          description: packageJson.description,
+        },
+      },
+    }),
+  )
+  .use(opentelemetry())
+  .use(
+    staticPlugin({
+      assets: "src/assets",
+      prefix: "/",
+      noCache: process.env.NODE_ENV === "development",
+    }),
+  )
+  .use(indexController)
+  .use(uploadFilesController);
 
 export type App = typeof server;
