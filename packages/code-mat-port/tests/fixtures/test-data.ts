da1b8451a1c0b74c0d023413c97ec4080c2cebc2
Juan bigorra <10943260+jbigorra@users.noreply.github.com>
2026-03-07 15:27:03 +0100

    chore: add shared types and test fixtures

---
+++ packages/code-mat-port/tests/fixtures/test-data.ts
@@ -0,0 +1,22 @@
+import type { VCSEntry, AnalysisOptions } from "../../src/code_maat/types";
+
+export const vcs: VCSEntry[] = [
+  { author: "apt", entity: "A", rev: 1, message: "Some change" },
+  { author: "apt", entity: "B", rev: 1, message: "Another change" },
+  { author: "apt", entity: "A", rev: 2, message: "Second change" },
+  { author: "jt",  entity: "A", rev: 3, message: "Third change" },
+];
+
+export const singleVcs: VCSEntry[] = [
+  { author: "apt", entity: "A", rev: 1 },
+];
+
+export const emptyVcs: VCSEntry[] = [];
+
+export const optionsWithLowThresholds: AnalysisOptions = {
+  minRevs: 1,
+  minSharedRevs: 1,
+  minCoupling: 50,
+  maxCoupling: 100,
+  maxChangesetSize: 10,
+};
