aaa7d83896e586e39d35ded6a0cdb1c5336a50b3
Juan bigorra <10943260+jbigorra@users.noreply.github.com>
2026-04-16 21:45:46 +0200

    refactor(charts): migrate ownership-chart.visual.ts to defineDomainChart factory

--- packages/charts/src/domain/ownership-chart.visual.ts
+++ packages/charts/src/domain/ownership-chart.visual.ts
@@ -1,13 +1,9 @@
 import type { EntityOwnership } from "@prj-conq/behave";
-import { html, LitElement } from "lit";
-import { customElement, property } from "lit/decorators.js";
-import { DataFetchController } from "../controllers/data-fetch.controller";
+import { html } from "lit";
 import { mapOwnershipToDoughnut, mapOwnershipToStacked } from "../mappers/ownership.mapper";
-import type { ThemePreset } from "../types";
-import "../generic/stacked-bar.visual";
+import { defineDomainChart } from "./define-domain-chart";
 import "../generic/doughnut.visual";
-
-type OwnershipVariant = "stacked" | "doughnut";
+import "../generic/stacked-bar.visual";
 
 /**
  * Entity ownership chart showing author contributions as stacked bar or doughnut.
@@ -24,41 +20,24 @@ type OwnershipVariant = "stacked" | "doughnut";
  * <pq-ownership-chart src="/api/analysis/ownership" variant="stacked"></pq-ownership-chart>
  * ```
  */
-@customElement("pq-ownership-chart")
-export class PqOwnershipChart extends LitElement {
-  private fetcher = new DataFetchController<EntityOwnership>(this);
-
-  @property({ type: Array }) data?: EntityOwnership[];
-  @property() src?: string;
-  @property() theme?: ThemePreset;
-  @property() variant: OwnershipVariant = "stacked";
-  @property() entity = "";
-
-  protected override async updated(changed: Map<string, unknown>): Promise<void> {
-    if (changed.has("src") || changed.has("data"))
-      await this.fetcher.fetch(this.src ?? "", !!this.data);
-  }
-
-  private get resolvedData(): EntityOwnership[] {
-    return this.data ?? this.fetcher.data ?? [];
-  }
-
-  protected override render() {
-    if (this.variant === "doughnut") {
-      return html`<pq-doughnut
-        .data=${mapOwnershipToDoughnut(this.resolvedData, this.entity)}
-        .theme=${this.theme}
-      ></pq-doughnut>`;
-    }
-    return html`<pq-stacked-bar
-      .data=${mapOwnershipToStacked(this.resolvedData)}
-      .theme=${this.theme}
-    ></pq-stacked-bar>`;
-  }
-}
+export const PqOwnershipChart = defineDomainChart<EntityOwnership, { entity: string }>({
+  tag: "pq-ownership-chart",
+  defaultVariant: "stacked",
+  properties: { entity: {} },
+  defaults: { entity: "" },
+  variants: {
+    doughnut: (data, theme, _limit, extra) =>
+      html`<pq-doughnut
+        .data=${mapOwnershipToDoughnut(data, extra?.entity ?? "")}
+        .theme=${theme}
+      ></pq-doughnut>`,
+    stacked: (data, theme) =>
+      html`<pq-stacked-bar .data=${mapOwnershipToStacked(data)} .theme=${theme}></pq-stacked-bar>`,
+  },
+});
 
 declare global {
   interface HTMLElementTagNameMap {
-    "pq-ownership-chart": PqOwnershipChart;
+    "pq-ownership-chart": InstanceType<typeof PqOwnershipChart>;
   }
 }
