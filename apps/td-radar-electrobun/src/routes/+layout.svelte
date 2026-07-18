9e1308da13b596886fc32318ef11b1baeff6b6cc
Juan bigorra <10943260+jbigorra@users.noreply.github.com>
2026-07-18 11:53:04 +0200

    Feat: Integrate Carbon Design System for application layout

--- apps/td-radar-electrobun/src/routes/+layout.svelte
+++ apps/td-radar-electrobun/src/routes/+layout.svelte
@@ -1,7 +1,50 @@
 <script lang="ts">
-  import "../app.css";
+  import {
+    Breadcrumb,
+    BreadcrumbItem,
+    Content,
+    Grid,
+    Header,
+    Row,
+  } from "carbon-components-svelte";
+  import "carbon-components-svelte/css/g80.css";
+  import { page } from "$app/state";
 
   let { children } = $props();
+
+  let isSideNavOpen = $state(false);
+  let getSanitizedPaths = () => {
+    const isHomePage = page.url.pathname === "/";
+    if (isHomePage) return [""];
+    return page.url.pathname.split("/");
+  };
+  let breadcrumbItems = $derived(
+    getSanitizedPaths().map((segment, index, paths) => {
+      return {
+        href: `/${segment}`,
+        text:
+          index === 0
+            ? "Home"
+            : segment.charAt(0).toUpperCase() + segment.slice(1),
+        active: index === paths.length - 1,
+      };
+    }),
+  );
 </script>
 
-{@render children()}
+<Header companyName="" platformName="TD Radar" bind:isSideNavOpen>
+  <Breadcrumb>
+    {#each breadcrumbItems as item}
+      <BreadcrumbItem href={item.href} isCurrentPage={item.active}>
+        {item.text}
+      </BreadcrumbItem>
+    {/each}
+  </Breadcrumb>
+</Header>
+<Content>
+  <Grid>
+    <Row>
+      {@render children()}
+    </Row>
+  </Grid>
+</Content>
